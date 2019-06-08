e3323fa0e602718dd988e7c0777301caa6236d63
JamesSpiva <zspiva@gmail.com>
2019-06-08 15:29:22 -0500

    added null case to mapper

--- Neon_Icon/Data/Mapper.cs
+++ Neon_Icon/Data/Mapper.cs
@@ -9,69 +9,129 @@ namespace Data
 {
     public static class Mapper
     {
-        
-        public static DomainEntities.Genre Map(Entities.Genre g) => new DomainEntities.Genre
+
+        public static DomainEntities.Genre Map(Entities.Genre g)
         {
-            id = g.Id,
-            type = g.Type
-        };
-        public static Entities.Genre Map(DomainEntities.Genre g) => new Entities.Genre
+            if (g == null) { return null; }
+
+            return new DomainEntities.Genre
+            {
+                id = g.Id,
+                type = g.Type
+            };
+        }
+
+        public static Entities.Genre Map(DomainEntities.Genre g)
         {
-            Id = g.id,
-            Type = g.type
-        };
-        public static DomainEntities.Location Map(Entities.Locations l) => new DomainEntities.Location
+            if (g == null) { return null; }
+
+            return new Entities.Genre
+            {
+                Id = g.id,
+                Type = g.type
+            };
+        }
+
+        public static DomainEntities.Location Map(Entities.Locations l)
         {
-            id = l.Id,
-            location = l.Location
-        };
-        public static Entities.Locations Map(DomainEntities.Location l) => new Entities.Locations
+            if (l == null) { return null; }
+
+            return new DomainEntities.Location
+            {
+                id = l.Id,
+                location = l.Location
+            };
+        }
+
+        public static Entities.Locations Map(DomainEntities.Location l)
         {
-            Id = l.id,
-            Location = l.location
-        };
-        public static DomainEntities.Preference Map(Entities.Preferences p) => new DomainEntities.Preference
+            if (l == null) { return null; }
+
+            return new Entities.Locations
+            {
+                Id = l.id,
+                Location = l.location
+            };
+        }
+
+        public static DomainEntities.Preference Map(Entities.Preferences p)
         {
-            preference_id = p.Id,
-            user_id = p.UserId,
-            weather_id = p.WeatherId,
-            genre_id = p.GenreId
-        };
-        public static Entities.Preferences Map(DomainEntities.Preference p) => new Entities.Preferences
+            if (p == null) { return null; }
+
+            return new DomainEntities.Preference
+            {
+                preference_id = p.Id,
+                user_id = p.UserId,
+                weather_id = p.WeatherId,
+                genre_id = p.GenreId
+            };
+        }
+
+        public static Entities.Preferences Map(DomainEntities.Preference p)
         {
-            Id = p.preference_id,
-            UserId = p.user_id,
-            WeatherId = p.weather_id,
-            GenreId = p.genre_id
-        };
-        public static DomainEntities.User Map(Entities.Users u) => new DomainEntities.User
+            if (p == null) { return null; }
+
+            return new Entities.Preferences
+            {
+                Id = p.preference_id,
+                UserId = p.user_id,
+                WeatherId = p.weather_id,
+                GenreId = p.genre_id
+            };
+        }
+
+        public static DomainEntities.User Map(Entities.Users u)
         {
-            id = u.Id,
-            username = u.Username,
-            password = u.Password,
-            location_id = u.LocationId
-        };
-        public static Entities.Users Map(DomainEntities.User u) => new Entities.Users
+            if (u == null) { return null; }
+
+            return new DomainEntities.User
+            {
+                id = u.Id,
+                username = u.Username,
+                password = u.Password,
+                location_id = u.LocationId
+            };
+        }
+
+        public static Entities.Users Map(DomainEntities.User u)
         {
-            Id = u.id,
-            Username = u.username,
-            Password = u.password,
-            LocationId = u.location_id
-        };
-        public static DomainEntities.Weather Map(Entities.Weather w) => new DomainEntities.Weather
+            if (u == null) { return null; }
+
+            return new Entities.Users
+            {
+                Id = u.id,
+                Username = u.username,
+                Password = u.password,
+                LocationId = u.location_id
+            };
+        }
+
+        public static DomainEntities.Weather Map(Entities.Weather w)
         {
-            weather_id = w.Id,
-            type = w.Type,
-            description = w.Description,
-            default_genre = w.DefaultGenre
-        };
-        public static Entities.Weather Map(DomainEntities.Weather w) => new Entities.Weather
+            if (w == null) { return null; }
+
+            return new DomainEntities.Weather
+            {
+                weather_id = w.Id,
+                type = w.Type,
+                description = w.Description,
+                default_genre = w.DefaultGenre
+            };
+        }
+
+        public static Entities.Weather Map(DomainEntities.Weather w)
         {
-            Id = w.weather_id,
-            Type = w.type,
-            Description = w.description,
-            DefaultGenre = w.default_genre
-        };
+            if (w == null) { return null; }
+
+            return new Entities.Weather
+            {
+                Id = w.weather_id,
+                Type = w.type,
+                Description = w.description,
+                DefaultGenre = w.default_genre
+            };
+        }
+
         public static IEnumerable<DomainEntities.Genre> Map(IEnumerable<Entities.Genre> g) => g.Select(Map);
         public static IEnumerable<Entities.Genre> Map(IEnumerable<DomainEntities.Genre> g) => g.Select(Map);
         public static IEnumerable<DomainEntities.Location> Map(IEnumerable<Entities.Locations> l) => l.Select(Map);
