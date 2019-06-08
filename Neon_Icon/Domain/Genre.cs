274059bd414ac76986dd3c4a520f5edd6e6980ef
eruscalleda <emmanuel.ruscalleda@gmail.com>
2019-06-08 12:08:22 -0500

    user and genre domain classes added

---
+++ Neon_Icon/Domain/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Genre
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+    }
+}
---
+++ Neon_Icon/Domain/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public int location_id { get; set; }
+    }
+}
