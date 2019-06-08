bf8e23111880626260752ea9eea48fe5d69a3343
Nathan Woodruff <nwoodr94@gmail.com>
2019-06-08 12:07:25 -0500

    scaffolded database entities

---
+++ Neon_Icon/Data/Entities/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.Entities
+{
+    public partial class Users
+    {
+        public Users()
+        {
+            Preferences = new HashSet<Preferences>();
+        }
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int? LocationId { get; set; }
+
+        public virtual Locations Location { get; set; }
+        public virtual ICollection<Preferences> Preferences { get; set; }
+    }
+}
---
+++ Neon_Icon/Data/Entities/Weather.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.Entities
+{
+    public partial class Weather
+    {
+        public Weather()
+        {
+            Preferences = new HashSet<Preferences>();
+        }
+
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public int DefaultGenre { get; set; }
+
+        public virtual Genre DefaultGenreNavigation { get; set; }
+        public virtual ICollection<Preferences> Preferences { get; set; }
+    }
+}
