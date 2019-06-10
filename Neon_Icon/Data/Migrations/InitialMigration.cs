098239a8b9fc00280405d4fde4e0e0f2befaaa32
Nathan Woodruff <nwoodr94@gmail.com>
2019-06-10 13:54:45 -0500

    code first migrations added

---
+++ Neon_Icon/Data/Migrations/InitialMigration.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false),
+                    type = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    location = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Weather",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    type = table.Column<string>(maxLength: 50, nullable: true),
+                    description = table.Column<string>(maxLength: 50, nullable: true),
+                    default_genre = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weather", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__Weather__default__5070F446",
+                        column: x => x.default_genre,
+                        principalTable: "Genre",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    username = table.Column<string>(maxLength: 50, nullable: true),
+                    password = table.Column<string>(maxLength: 50, nullable: true),
+                    location_id = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__Users__location___4BAC3F29",
+                        column: x => x.location_id,
+                        principalTable: "Locations",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Preferences",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    user_id = table.Column<int>(nullable: false),
+                    weather_id = table.Column<int>(nullable: false),
+                    genre_id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Preferences", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__Preferenc__genre__5535A963",
+                        column: x => x.genre_id,
+                        principalTable: "Genre",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK__Preferenc__user___534D60F1",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK__Preferenc__weath__5441852A",
+                        column: x => x.weather_id,
+                        principalTable: "Weather",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Preferences_genre_id",
+                table: "Preferences",
+                column: "genre_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Preferences_user_id",
+                table: "Preferences",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Preferences_weather_id",
+                table: "Preferences",
+                column: "weather_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_location_id",
+                table: "Users",
+                column: "location_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Weather_default_genre",
+                table: "Weather",
+                column: "default_genre");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Preferences");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Weather");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+        }
+    }
+}
--- Neon_Icon/Domain/DomainEntities/User.cs
+++ Neon_Icon/Domain/DomainEntities/User.cs
@@ -9,7 +9,6 @@ namespace Domain.DomainEntities
         public int id { get; set; }
         public string username { get; set; }
         public string password { get; set; }
-        //public int? location_id { get; set; }
 
         public Location location { get; set; }
     }
