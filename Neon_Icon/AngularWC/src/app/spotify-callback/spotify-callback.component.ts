ed2cb936fc798f062813dc847957f6fb27bd1880
JamesSpiva <zspiva@gmail.com>
2019-06-24 10:07:24 -0500

    minor fixes

--- Neon_Icon/AngularWC/src/app/spotify-callback/spotify-callback.component.ts
+++ Neon_Icon/AngularWC/src/app/spotify-callback/spotify-callback.component.ts
@@ -20,21 +20,7 @@ export class SpotifyCallbackComponent implements OnInit {
       this.spotifyService.accessToken.token = params.get('access_token');
       this.spotifyService.accessToken.tokenType = params.get('token_type');
 
-<<<<<<< HEAD
       window.location.href="weather";
     });
-=======
-  public login(): void {
-    const scopes = new ScopesBuilder()/* .withScopes(ScopesBuilder.LIBRARY) */.build();
-    const ac: AuthConfig = {
-      client_id: '208a44d2f0e34e378facf5b39ddc6568',  // WebPortal App Id. Shoud be config
-      response_type: 'token',
-      redirect_uri: 'http://localhost:4200/spotifycallback',  // My URL
-      state: '',
-      show_dialog: true,
-      scope: scopes
-    };
-    this.authService.configure(ac).authorize();
->>>>>>> 7922269a4bdd8326de3e132640fd4f7693135055
   }
 }
