0ba3353520a85d59907725b9cc5baee7d2033683
FrozenBeans <guillermoetuyub@yahoo.com>
2019-06-24 19:29:22 -0500

    slamming development

--- Neon_Icon/AngularWC/src/app/user/user.component.ts
+++ Neon_Icon/AngularWC/src/app/user/user.component.ts
@@ -26,7 +26,6 @@ export class UserComponent implements OnInit {
       this.isLoginNotRegister = false;
   }
 
-<<<<<<< HEAD
   Login(username: string, password: string) {
     this.User = new User();
     this.User.username = username;
@@ -47,26 +46,5 @@ export class UserComponent implements OnInit {
       this.userService.Register(this.User)
         .subscribe(user => this.User = user)
     }
-=======
-  Submit(username: string, password: string, zip: string ) {
-    this.User = new User();
-    this.User.username = username;
-    this.User.password = password;
-    console.log(this.User);
-    if(this.isLoginNotRegister){
-      this.userService.Login(this.User)
-      .subscribe(user => this.User = user);
-    } else {
-      if (zip.length !== 5) {
-        alert('Please enter a valid ZIP Code');
-      } else {
-      this.User.zip = zip;
-      this.userService.Register(this.User)
-        .subscribe(user => this.User = user);
-      }
-    }
-
-
->>>>>>> 16fb6f36764defcbd9f2f883af289feac78afae8
   }
 }
