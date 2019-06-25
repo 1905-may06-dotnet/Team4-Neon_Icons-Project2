75674ab0269876d4585f04dcaa3c3a0b1660e75c
michaelbeneamatobruno <michaelbeneamatobruno@gmail.com>
2019-06-25 09:50:33 -0500

    adding embedded player

--- Neon_Icon/AngularWC/src/app/spotify/spotify.component.ts
+++ Neon_Icon/AngularWC/src/app/spotify/spotify.component.ts
@@ -10,12 +10,18 @@ import { Token } from '../access-token';
 })
 export class SpotifyComponent implements OnInit {
 
+  baseUrl = "https://open.spotify.com/embed/";
+
   constructor(private spotifyService: SpotifyService) { }
 
   ngOnInit() {
     window.addEventListener(' SpotifyWebPlaybackSDKReady', function(){
       console.log('sdkready');
     });
+  }
+
+  getUrl(): string {
+    return this.baseUrl + "https://open.spotify.com/playlist/37i9dQZF1DX1BzILRveYHb";
   }
 
   login() {
