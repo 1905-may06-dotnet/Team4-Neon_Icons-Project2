cacbe04fd4fa7e29a7b9f689f2be0a451213e212
Nathan Woodruff <nwoodr94@gmail.com>
2019-06-24 20:50:59 -0500

    some unit tests resolved

--- Neon_Icon/AngularWC/src/app/spotify-callback/spotify-callback.component.spec.ts
+++ Neon_Icon/AngularWC/src/app/spotify-callback/spotify-callback.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { SpotifyCallbackComponent } from './spotify-callback.component';
+
 
 describe('SpotifyCallbackComponent', () => {
   let component: SpotifyCallbackComponent;
