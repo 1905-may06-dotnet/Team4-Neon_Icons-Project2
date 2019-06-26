42c5199920e8ffa639bae163813c5ff4e21c2764
michaelbeneamatobruno <michaelbeneamatobruno@gmail.com>
2019-06-25 19:44:39 -0500

    adding tests for backend

--- Neon_Icon/Test/UserTest.cs
+++ Neon_Icon/Test/UserTest.cs
@@ -12,9 +12,10 @@ namespace Test
     public class UserTest
     {
         Dummydb db = new Dummydb();
+        private readonly IUserRepository udb;
         public UserTest()
         {
-            
+            udb = new Data.Repositories.UserRepository();
         }
         [TestMethod]
         public void Find()
@@ -22,13 +23,22 @@ namespace Test
             Assert.IsTrue("testUsername0" == db.mockUser.Object.Find(0).username);
             
         }
+        [TestMethod]
         public void Create_validUser_NotNull()
         {
-
+            Domain.DomainEntities.User testUser = new Domain.DomainEntities.User();
+            testUser.username = "backendTest";
+            testUser.password = "password";
+            testUser.location = "98908";
+            udb.Create(testUser);
+            Assert.IsTrue(udb.Find(testUser.username).username == "backendTest");
         }
+        [TestMethod]
         public void Delete_validUser_Null()
         {
-
+            Domain.DomainEntities.User testUser = udb.Find("backendTest");
+            udb.Delete(testUser);
+            Assert.IsTrue(udb.Find(testUser.id) == null);
         }
     }
 }
--- Neon_Icon/Test/WeatherTest.cs
+++ Neon_Icon/Test/WeatherTest.cs
@@ -6,8 +6,36 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Test
 {
+    [TestClass]
     public class WeatherTest
     {
-        
+        private readonly IWeatherRepository wdb;
+        public WeatherTest(IWeatherRepository wdb)
+        {
+            this.wdb = wdb;
+        }
+
+        [TestMethod]
+        public void Create_validWeather_NotNull()
+        {
+            Domain.DomainEntities.Weather testWeather = new Domain.DomainEntities.Weather();
+            testWeather.type = "testType";
+            testWeather.description = "testDescription";
+            testWeather.default_genre = "testGenre";
+            wdb.CreateWeather(testWeather);
+            Assert.IsNotNull(wdb.GetWeather(testWeather));
+        }
+
+        [TestMethod]
+        public void Delete_validWeather_Null()
+        {
+            Domain.DomainEntities.Weather testWeather = new Domain.DomainEntities.Weather();
+            testWeather.type = "testType";
+            testWeather.description = "testDescription";
+            testWeather.default_genre = "testGenre";
+            wdb.DeleteWeather(wdb.GetWeather(testWeather));
+            Assert.IsNull(wdb.GetWeather(testWeather));
+        }
+
     }
 }
