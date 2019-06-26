75031b8978bc85b6ca553d3b67d42d5c1c6ce5f9
JamesSpiva <zspiva@gmail.com>
2019-06-25 21:35:48 -0500

    user login changes

--- Neon_Icon/AngularWC/src/app/user.service.ts
+++ Neon_Icon/AngularWC/src/app/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 
 import { MessageService } from './message.service';
@@ -7,6 +7,7 @@ import { catchError, map, tap } from 'rxjs/operators';
 
 
 import {User } from './user';
+import { MatCalendarBody } from '@angular/material';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -32,12 +33,15 @@ export class UserService {
 
   Login(user: User) {
     const url = this.userUrl + '/loginuser';
-    return this.http.post<User>(url, user, httpOptions)
+    this.http.post<User>(url, user, {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      observe: 'response'
+    })
       .pipe(
         tap(_ => this.log('Login User')),
         catchError(this.handleError<User>('Login'))
       )
-      .subscribe();
+      .subscribe(x => {if (x && x["status"] == 200) this.user = user;});
   }
 
   Logout() {
@@ -52,7 +56,7 @@ export class UserService {
         tap(_ => this.log('Register User')),
         catchError(this.handleError<User>('Register'))
       )
-      .subscribe();
+      .subscribe(x => this.user = user);
   }
 
   UpdateLocation(zip: string) {
@@ -63,7 +67,7 @@ export class UserService {
         tap(_ => this.log('Update User Location')),
         catchError(this.handleError<User>('Update Location'))
       )
-      .subscribe(user => this.user = user);
+      .subscribe();
   }
 
 
